391999d44ffd206e4e6729459e8eb4c74ed4ff0e
Ritwik <sritwik03@gmail.com>
2022-01-27 18:45:17 +0530

    testing

--- test.py
+++ test.py
@@ -6,18 +6,15 @@ from ball_red_det import ball_red_det
 from ball_green_det import ball_green_det
 
 img = cv2.imread('ball.jpeg')
-imutils.resize(img, width=400)
-# cv2.imshow('img', img)
+img = imutils.resize(img, width=500)
 
-ball_blue_det(img)
-# print(center)
+center_green = ball_green_det(img)
+center_red = ball_red_det(img)
+center_blue = ball_blue_det(img)
+print(center_red, center_green, center_blue)
 
-# cv2.imshow('new_img', new_img)
-# key = cv2.waitKey(0) & 0xFF
-# if key == 'q':
-#     cv2.destroyAllWindows()
-
-# key = cv2.waitKey(0) & 0xFF
-# if key == ord('q'):
-#     cv2.destroyAllWindows()
+cv2.imshow('img', img)
+key = cv2.waitKey(0) & 0xFF
+if key == 'q':
+    cv2.destroyAllWindows()
 
